0c2a16771a03e495bf22f8471f4c2ad98db0a642
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2025-01-22 19:52:53 +0700

    what is delegate?

--- HocDelegate/Form1.cs
+++ HocDelegate/Form1.cs
@@ -10,6 +10,15 @@ using System.Windows.Forms;
 
 namespace HocDelegate
 {
+    /*
+     * Delegate hoạt động như một "tập hợp" chứa nhiều biến thể của một hàm.
+        Các phương thức (biến thể) được gọi tuần tự theo thứ tự anh thêm vào.
+        Dùng delegate khi:
+        Cần gọi nhiều phương thức khác nhau trong một luồng xử lý.
+        Muốn viết code linh hoạt và dễ mở rộng.
+     *  - nhưng cần có chung datatype trả về và tham số
+     * 
+     */
     public partial class Form1 : Form
     {
         public Form1()
