b3fd7e189b61d88e2df5b2a8aebe9220be228041
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2025-02-16 07:13:21 +0700

    31. Skip

--- HocSkip/Form1.cs
+++ HocSkip/Form1.cs
@@ -12,6 +12,9 @@ namespace HocSkip
 {
     public partial class Form1 : Form
     {
+        /*
+         * Skip: bỏ qua n phần tử đầu tiên
+         */
         public Form1()
         {
             InitializeComponent();
@@ -20,11 +23,11 @@ namespace HocSkip
         Random rd = new Random();
         private void btnTaoDS_Click(object sender, EventArgs e)
         {
-            int n = int.Parse(txtN.Text);
+            int n = int.Parse(txtN.Text); // số phần tử của ds
             dsInt.Clear();
             for(int i=0;i<n;i++)
             {
-                int x = rd.Next(100);
+                int x = rd.Next(100);// sinh ngẫu nhiên số nguyên từ 0 đến 99
                 dsInt.Add(x);
             }
             lstGoc.Items.Clear();
@@ -33,10 +36,10 @@ namespace HocSkip
 
         private void btnSkip_Click(object sender, EventArgs e)
         {
-            int n = int.Parse(txtSkip.Text);
-            var dsMoi = dsInt.Skip(n);
-            lstSkip.Items.Clear();
-            dsMoi.ToList().ForEach(x => lstSkip.Items.Add(x));
+            int n = int.Parse(txtSkip.Text); // số phần tử bỏ qua
+            var dsMoi = dsInt.Skip(n); // bỏ qua n phần tử đầu tiên
+            lstSkip.Items.Clear();// xóa hết dữ liệu cũ
+            dsMoi.ToList().ForEach(x => lstSkip.Items.Add(x));// thêm dữ liệu mới
         }
     }
 }
