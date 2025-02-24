d5786abdf1a8ee10744b206d99d235e323f069a6
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2025-02-24 12:29:42 +0700

    35. ToArray

--- HocToArray/Form1.cs
+++ HocToArray/Form1.cs
@@ -12,6 +12,8 @@ namespace HocToArray
 {
     public partial class Form1 : Form
     {
+        /*chuyển danh sách (List<T>) hoặc tập hợp (IEnumerable<T>) 
+         * thành một mảng (T[]).*/
         public Form1()
         {
             InitializeComponent();
@@ -19,16 +21,31 @@ namespace HocToArray
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            // Tạo danh sách số nguyên
             List<int> dsInt = new List<int>();
-            
-            dsInt.AddRange(new int[] { 5,7,1,0,35});
+
+            // Thêm nhiều phần tử vào danh sách
+            dsInt.AddRange(new int[] { 5, 7, 1, 0, 35 });
+
+            // Chuyển đổi danh sách List<int> sang mảng int[]
             var arr = dsInt.ToArray();
+
+            // Xóa nội dung cũ trong Label trước khi hiển thị
             lblKetQua.Text = "";
-            for(int i=0;i<arr.Length;i++)
+
+            // Duyệt qua từng phần tử trong mảng và nối vào chuỗi kết quả
+            for (int i = 0; i < arr.Length; i++)
             {
-                lblKetQua.Text += arr[i]+" ";
+                lblKetQua.Text += arr[i] + " ";
             }
         }
+
+
+        /*
+         * ToArray() giúp chuyển đổi danh sách (List<T>) hoặc tập hợp (IEnumerable<T>) thành mảng (T[]).
+        Dữ liệu được sao chép nên không ảnh hưởng đến danh sách gốc.
+        Dùng khi cần mảng thay vì danh sách động.
+        Có thể kết hợp với LINQ để xử lý dữ liệu trước khi chuyển đổi.
+         */
     }
 }
